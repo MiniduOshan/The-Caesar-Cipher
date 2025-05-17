861dee27be9b048d5848eca1d6038c517c76afdb
chamindu irosh <chaminduirosh927@gmail.com>
2025-05-17 19:31:59 +0530

    variable declarations

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
-// main.c
+// Main program for Caesar Cipher using Circular Queue
+
 #include <stdio.h>
 #include <string.h>
 #include "cipher.h"
 
 int main() {
-    return 0;
+    char choice;           // User's choice: E for encryption, D for decryption
+    char message[1000];    // Input message from the user
+    char result[1000];     // Result after encryption/decryption
+    int shift;             // Shift key value
+    char repeat;           // User's choice to repeat the process
 }
