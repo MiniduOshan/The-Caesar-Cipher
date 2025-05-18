dbd6e70c483d319007d3f240d1a03eaee0b397b3
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2025-05-18 23:09:53 +0530

    Update cipher.c

--- cipher.c
+++ cipher.c
@@ -1,9 +1,5 @@
-/*
-File_Name: cipher.c
-Index    :2022t01584 
-Name     : B.D.M.O.B.Wanigarathna 
-*/
-
+//Index : 2022t01584 
+//Name  : B.D.M.O.B. Wanigarathna 
 
 #include <stdio.h>
 #include <stdlib.h>
